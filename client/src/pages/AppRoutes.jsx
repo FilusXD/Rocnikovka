511117469dd72d132c5425c3bdf3a4062879b90d
filip M <filipmasek1202@seznam.cz>
2024-12-18 15:27:43 +0100

    update1.5

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -4,6 +4,7 @@ import ProductCreateForm from "./ProductCreateForm";
 import CreatedProduct from "./ProductCreateForm/CreatedProduct";
 import ProductList from "./ProductList";
 import ProductView from "./ProductView";
+import ProductUpdateForm from "./ProductUpdateForm";
 
 export default function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ export default function AppRoutes() {
         <Route path="/created-product/:id" element={<CreatedProduct/>}/>
         <Route path="/view-products" element={<ProductList/>}/>
         <Route path="/product/:id" element={<ProductView/>}/>
+        <Route path="/update-product/:id" element={<ProductUpdateForm/>}/>
     </Routes>
     </BrowserRouter>
     </>
