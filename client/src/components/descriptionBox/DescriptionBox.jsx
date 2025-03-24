fcf6daf79aaebb5054642be1d956535d3437a596
filip M <filipmasek1202@seznam.cz>
2025-03-24 10:38:35 +0100

    Cart functions

--- client/src/components/descriptionBox/DescriptionBox.jsx
+++ client/src/components/descriptionBox/DescriptionBox.jsx
@@ -4,7 +4,14 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor est molestiae adipisci officia rem magnam cum corrupti alias ratione dicta nemo, eveniet eius, laudantium, doloremque quibusdam sunt quaerat. Deleniti, voluptatem!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque temporibus minima neque ullam nihil modi, labore maxime perspiciatis enim ea nobis itaque voluptatum explicabo asperiores quibusdam officiis ipsam iure quasi.</p>
+      </div>
     </div>
   )
 }
